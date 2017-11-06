c848e1e9736e6687a100021a612b8aefce36f026
Arthur Dubedat <arthur.dubedat@gmail.com>
2017-11-06 15:30:05 +0100

    error read and write 2 char fixed

--- Parsing.ml
+++ Parsing.ml
@@ -133,9 +133,9 @@ let new_transition (obj: Yojson.Basic.json) =
     let to_state = obj |> member "to_state" |> to_string in
     let write = obj |> member "write" |> to_string in
     let action = obj |> member "action" |> to_string in
-    if String.length read = 1 && List.mem read.[0] alphabet = false then (print_endline "Error: read element must be in alphabet list"; exit 0)
+    if String.length read <> 1 || List.mem read.[0] alphabet = false then (print_endline "Error: read element must be in alphabet list"; exit 0)
     else if List.mem to_state states = false then (print_endline "Error: to_state element must be in states list"; exit 0)
-    else if String.length write = 1 && List.mem write.[0] alphabet = false then (print_endline "Error: write element must be in alphabet list"; exit 0)
+    else if String.length write <> 1 || List.mem write.[0] alphabet = false then (print_endline "Error: write element must be in alphabet list"; exit 0)
     else if action <> "RIGHT" && action <> "LEFT" then (print_endline "Error: action element not well formatted"; exit 0)
     else begin
         let act = if action = "RIGHT" then Right else Left in
